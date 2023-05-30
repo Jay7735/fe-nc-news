c43526e6f1245b978ca783620580a62cf3161732
Jay7735 <jaydeepdhillon@hotmail.co.uk>
2023-05-30 14:32:14 +0100

    should make all articles display

--- fe-nc-news/src/App.jsx
+++ fe-nc-news/src/App.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Articles from './Articles'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>NC News</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        The only social news website you will need.
       </p>
+      <BrowserRouter>
+      <div className="buttonZone">
+          <Link to='/articles'>
+            <button className="button-45">Articles</button>
+          </Link>
+        </div>
+        
+        <Routes>
+          <Route path="/articles" element={<Articles />} />
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
